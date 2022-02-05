9ef72bcb63de9e142e96f1a7d409cefa86fe36a4
Sensio <89518717+SensioX@users.noreply.github.com>
2022-02-04 16:45:19 -0800

    Add files via upload

---
+++ misc.go
@@ -0,0 +1,51 @@
+// Copyright (C) 2021 github.com/V4NSH4J
+//
+// This source code has been released under the GNU Affero General Public
+// License v3.0. A copy of this license is available at
+// https://www.gnu.org/licenses/agpl-3.0.en.html
+
+package utilities
+
+import "fmt"
+
+func Contains(s []string, e string) bool {
+	defer HandleOutOfBounds()
+	if len(s) == 0 {
+		return false
+	}
+	for _, a := range s {
+		if a == e {
+			return true
+		}
+	}
+	return false
+}
+
+// Inputs 2 slices of strings and returns a slice of strings which does not contain elements from the second slice
+func RemoveSubset(s []string, r []string) []string {
+	var n []string
+	for _, v := range s {
+		if !Contains(r, v) {
+			n = append(n, v)
+		}
+	}
+	return n
+}
+
+func RemoveDuplicateStr(strSlice []string) []string {
+	allKeys := make(map[string]bool)
+	list := []string{}
+	for _, item := range strSlice {
+		if _, value := allKeys[item]; !value {
+			allKeys[item] = true
+			list = append(list, item)
+		}
+	}
+	return list
+}
+
+func HandleOutOfBounds() {
+	if r := recover(); r != nil {
+		fmt.Printf("Recovered from Panic %v", r)
+	}
+}
